7f4c875410f8b7d06052eddf5827655e186106b4
ChristopherLee8 <53844978+ChristopherLee8@users.noreply.github.com>
2022-03-31 20:42:09 +1100

    added final stages for Assignment

--- DSClient.java
+++ DSClient.java
@@ -15,12 +15,47 @@ public class DSClient {
             boolean found = false;
             String currentMsg = "";
             CSH(s); //Client/Server Handshake
-            
-            sendMsg(s, "HELO\n"); //Sends HELO to server
-            currentMsg = readMsg(s); //Displays received message
-            sendMsg(s, "BYE\n"); //Sends BYE to server
-            currentMsg = readMsg(s); //Displays received message
 
+
+            // This while loop will run until
+            while (!currentMsg.contains("NONE")) {
+                
+                sendMsg(s, "REDY\n"); //sends message to the server saying its ready for jobs
+                currentMsg = readMsg(s);
+                
+                // Checks to see if the message is a new job
+                if (currentMsg.contains("JOBN")) {
+                    String[] JOBNSplit = currentMsg.split(" ");
+                    //checks which servers are availble
+                    sendMsg(s, "GETS Avail " + JOBNSplit[4] + " " + JOBNSplit[5] + " " + JOBNSplit[6] + "\n");
+                    //Reads the msg saying what data is about to be sent and responds with "OK"
+                    currentMsg = readMsg(s);
+                    sendMsg(s, "OK\n");
+
+                    currentMsg = readMsg(s);
+                    sendMsg(s, "OK\n");
+
+                    //Checks to see if the largest Server has been found
+                    //Used as a flag to see that it was found on the first round
+                    if(found == false){
+                        ls = findLS(currentMsg);
+                        found = true;
+                    }
+                    currentMsg = readMsg(s); //reads . from the message
+
+                    //Schedule the current job to the largest server (SCHD JobNumber ServerName ServerNumber)
+                    sendMsg(s, "SCHD " + JOBNSplit[2] + " " + ls[0] + " " + ls[1] + "\n");
+
+                    //Read the next JOBN
+                    currentMsg = readMsg(s);
+                    System.out.println("SCHD: " + currentMsg);
+                }
+                else if (currentMsg.contains("DATA")) {
+                    sendMsg(s, "OK\n");
+                }
+            }
+            // Sends "Quit" to the server to end the session which then closes the socket
+            sendMsg(s, "QUIT\n");
             s.close();
         }
         catch (Exception e) {
@@ -69,4 +104,29 @@ public class DSClient {
         catch (IOException e) {e.printStackTrace();}
     }
 
+    public static String[] findLS(String currentMsg) {
+        // All the servers in the currentMsg being split into an array
+        String[] serversAndInfo = currentMsg.split("\n");
+        //Sets up variables
+        int mostCores = 0;
+        String[] currentServer = {""};
+        //Searches for the most cores a server holds in the given available servers
+        for (int i = 0; i < serversAndInfo.length; i++) {
+            currentServer = serversAndInfo[i].split(" ");
+            int currentCores = Integer.valueOf(currentServer[4]);
+            if (currentCores > mostCores) {
+                mostCores = currentCores;
+            }
+        }
+        //Finds and returns the biggest server (The one with the most cores)
+        for (int i = 0; i < serversAndInfo.length; i++) {
+            currentServer = serversAndInfo[i].split(" ");
+            int currentCores = Integer.valueOf(currentServer[4]);
+            if (currentCores == mostCores) {
+                return currentServer;
+            }
+        }
+        return currentServer;
+
+    }
 }
