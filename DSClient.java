0a1cc58db59b328fa1e889501be5893ccce0966e
ChristopherLee8 <53844978+ChristopherLee8@users.noreply.github.com>
2022-03-31 19:08:29 +1100

    Made Client more robust
    Added future variables for Final project

--- DSClient.java
+++ DSClient.java
@@ -4,38 +4,47 @@ import java.util.concurrent.*;
 
 public class DSClient {
     public static void main(String[] args) {
-        while(true) {
-            try{
-                InetAddress aHost = InetAddress.getByName(args[0]);
-                int aPort = Integer.parseInt(args[1]);
-                Socket s = new Socket(aHost,aPort);
-                DataOutputStream dout = new DataOutputStream(s.getOutputStream());
-                BufferedReader din = new BufferedReader(new InputStreamReader(s.getInputStream()));
 
-                System.out.println("Target IP: " + s.getInetAddress() + " Target Port: "+ s.getPort());
-                System.out.println("Local IP: " + s.getLocalAddress() + " Local Port: " + s.getLocalPort());
-                
-                dout.flush();
-                dout.write(("HELO\n").getBytes());
-                System.out.println("SENT: HELO");
-                dout.flush();
-                
-                String str = (String)din.readLine();
-                System.out.println("RCVD: "+str);
-                dout.write(("BYE\n").getBytes());
-                System.out.println("SENT: BYE");
-                dout.flush();
+        try{
+            InetAddress aHost = InetAddress.getByName(args[0]); //gets host from input
+            int aPort = Integer.parseInt(args[1]); //gets port from input
+            Socket s = new Socket(aHost,aPort); //assigns socket
 
-                str = (String)din.readLine();
-                System.out.println("RCVD: "+str);
-                dout.flush();
+            //variables
+            String[] ls = {""}; //Largest Server
+            boolean found = false;
+            String currentMsg = "";
+            
 
-                din.close();
-                dout.close();
-                s.close();
-            }
-            catch(Exception e){System.out.println(e);}
-            try {TimeUnit.SECONDS.sleep(1);} catch(InterruptedException e){System.out.println(e);}
+            DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+            BufferedReader din = new BufferedReader(new InputStreamReader(s.getInputStream()));
+
+            System.out.println("Target IP: " + s.getInetAddress() + " Target Port: "+ s.getPort());
+            System.out.println("Local IP: " + s.getLocalAddress() + " Local Port: " + s.getLocalPort());
+            
+            dout.flush();
+            dout.write(("HELO\n").getBytes());
+            System.out.println("SENT: HELO");
+            dout.flush();
+            
+            String str = (String)din.readLine();
+            System.out.println("RCVD: "+str);
+            dout.write(("BYE\n").getBytes());
+            System.out.println("SENT: BYE");
+            dout.flush();
+
+            str = (String)din.readLine();
+            System.out.println("RCVD: "+str);
+            dout.flush();
+
+            din.close();
+            dout.close();
+            s.close();
+            
+
+        }
+        catch (Exception e) {
+            System.out.println(e);
         }
     }
 }
